e2efd2427108b030027748250707449c6603343d
ObadaAmarneh <obada.amarneh20@gmail.com>
2022-04-30 13:24:54 +0300

    release 0

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ export default {
     htmlAttrs: {
       lang: 'en',
     },
+    // release_0
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },

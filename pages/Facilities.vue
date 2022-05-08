760d36c8c259a18f31aac0a2f13675c70f123b39
ObadaAmarneh <obada.amarneh20@gmail.com>
2022-05-08 13:19:58 +0300

    tt

---
+++ pages/Facilities.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <h1> walid     </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style>
+.header {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+h1 {
+  font-size: 78px;
+}
+</style>
